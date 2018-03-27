3a32e54e9210d72c4160d9811eb5ccb8b5ee75ae
Nohbdys <Billen93@outlook.com>
2018-03-27 13:23:11 +0200

    ReUploading

--- Source/DaniaUEProjekt/ExitTrigger.cpp
+++ Source/DaniaUEProjekt/ExitTrigger.cpp
@@ -1,20 +1,20 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "SoundTrigger.h"
+#include "ExitTrigger.h"
 
 
 // Sets default values for this component's properties
-USoundTrigger::USoundTrigger()
+UExitTrigger::UExitTrigger()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
+	// ...
 }
 
 
 // Called when the game starts
-void USoundTrigger::BeginPlay()
+void UExitTrigger::BeginPlay()
 {
 	Super::BeginPlay();
 
@@ -24,13 +24,13 @@ void USoundTrigger::BeginPlay()
 
 
 // Called every frame
-void USoundTrigger::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UExitTrigger::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
+
 	if (pressurePlate && pressurePlate->IsOverlappingActor(triggerType))
 	{
-		onSoundRequest.Broadcast();
+		onExitRequest.Broadcast();
 	}
 }
 
